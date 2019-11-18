1cc8f92bdadc530fb2b5717210ea8e1c358d74bf
Tuomas Karna <tuomas.karna@gmail.com>
2019-11-18 10:44:24 +0200

    add templates for run17

--- template/namelist/cal2014-17/env_path.sh
+++ template/namelist/cal2014-17/env_path.sh
@@ -3,11 +3,11 @@
 #
 
 # root directory where all individual run dirs will be created
-RUNTAG=run17
+RUNTAG=run15
 RUN_ROOT_DIR=../../$RUNTAG
 
 # choose which namelist template to use
-NMLCONFIG=cal2014-17
+NMLCONFIG=cal2014-15
 
 # config specific flags
 FLAG_WAVE=.false.
@@ -25,7 +25,7 @@ RUNDIR_PREFIX='run'
 # directory that contains all the necessary input files
 # - netcdf files, *.xml files, nemo xios binary files
 # NOTE: do not include namelist files here
-CFG_TEMPLATE_DIR=$RUN_ROOT_DIR/../run_template_17/
+CFG_TEMPLATE_DIR=$RUN_ROOT_DIR/../run_template_15/
 
 # time span to execute, start date of first/last run
 start_date="2014-10-01"
@@ -35,5 +35,5 @@ end_date="2014-12-01"
 init_date="2014-10-01"
 
 # hotstart option
-HOTSTART=1
-HOTSTART_DIR='/lustre/tmp/karna/runs/bal-mfc/NemoNordic/reference/smhi_cal_2014/init_state/run17-init_2014-10-01/'
+HOTSTART=0
+HOTSTART_DIR=''
