937517d4ed6418390179ed93b71f2ee55769cca9
Tuomas Karna <tuomas.karna@gmail.com>
2020-06-29 10:24:29 +0300

    add template for smhi calval run34

---
+++ template/namelist/cal2014-34/env_path.sh
@@ -0,0 +1,44 @@
+#
+# Set directory paths for NEMO run
+#
+
+# root directory where all individual run dirs will be created
+RUNTAG=run34
+RUN_ROOT_DIR=../../$RUNTAG
+
+# choose which namelist template to use
+NMLCONFIG=cal2014-34
+
+# config specific flags
+FLAG_WAVE=.false.
+FLAG_WDRAG=.false.
+FLAG_STOKES=.false.
+FLAG_NTAUW=.false.
+FLAG_TAUW=.false.
+
+# modification to namelist_cfg or ref
+SINGLE_NML=true
+
+# prefix to be used in run directory names, e.g. run_2016-06
+RUNDIR_PREFIX='run'
+
+# directory that contains all the necessary input files
+# - netcdf files, *.xml files, nemo xios binary files
+# NOTE: do not include namelist files here
+CFG_TEMPLATE_DIR=$RUN_ROOT_DIR/../run_template_34/
+
+QUEUE_MANAGER=slurm
+
+# time span to execute, start date of first/last run
+start_date="2014-10-01"
+end_date="2016-09-01"
+
+# cold/hot start date
+init_date="2014-10-01"
+
+# parent run: first run will wait for this to end
+MASTER_PARENT_JOB=""
+
+# hotstart option
+HOTSTART=0
+HOTSTART_DIR=''
